9f992e2db95f561d203d43819239bf8a2787a0d5
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2024-04-05 14:51:16 +0200

    Display dates in human-readable format

--- src/ViewHelpers/ViewHelper.php
+++ src/ViewHelpers/ViewHelper.php
@@ -19,4 +19,15 @@ class ViewHelper
 
     include $path;
   }
+
+    /**
+     * Converts a given date string to a human-readable format.
+     *
+     * @param string $date The date string to be converted.
+     * @return string The converted date in the format "Month day, Year".
+     */
+  public function humanDate(string $date): string
+  {
+      return date('F j, Y', strtotime($date));
+  }
 }
